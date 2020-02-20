3c2d92e131759c4148330cb71de4b7b017dba8fc
Antoine <grugeon.antoine@gmail.com>
2020-02-20 23:14:10 +0100

    Blink avec check du timing et combo
    Avec musique test

--- GPK Project/Assets/Scripts/Managers/BeatManager.cs
+++ GPK Project/Assets/Scripts/Managers/BeatManager.cs
@@ -4,28 +4,58 @@ using UnityEngine;
 public class BeatManager : MonoBehaviour
 {
     #region Initialization
-    public int bpm;
+    [Range(1, 400)] public float bpm;
     [Tooltip("L'intervalle de temps dont le joueur dispose pour effectuer son action et être en rythme.")]
     [Range(0f,1f)] public float timingThreshold = 0.2f;
     [Range(-1f,1f)] public float timingThresholdOffset;
+    [Range(0f, 1f)] public float beatStartTimeOffset;
 
+    public float cameraBeatEffectLerpSpeed;
+    public float cameraBeatEffectAmplitude;
+
+
+    private bool musicStarted;
     private float beatTime;
     private float timeBeforeNextBeat;
+    private float currentBeatProgression;
     private float nextBeatStartTime;
+    private float offBeatStartTime;
+
+    private AudioSource source;
+
+    private float initialCameraSize;
+    private Camera mainCamera;
     #endregion
 
 
     void Start()
     {
+        source = GetComponent<AudioSource>();
+        musicStarted = false;
         beatTime = 60 / bpm;
-        timeBeforeNextBeat = beatTime;
-        nextBeatStartTime = Time.time;
+
+        mainCamera = Camera.main;
+        initialCameraSize = mainCamera.orthographicSize;
     }
 
-
-    void Update()
+    private void Update()
     {
-        TimeCycle();
+        if(!musicStarted && Input.GetButtonDown("Blink"))
+        {
+            musicStarted = true;
+
+            source.Play();
+            nextBeatStartTime = Time.time + beatStartTimeOffset;
+            offBeatStartTime = nextBeatStartTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(musicStarted)
+        {
+            TimeCycle();
+        }
     }
 
     /// <summary>
@@ -36,7 +66,44 @@ public class BeatManager : MonoBehaviour
         if (nextBeatStartTime < Time.time)
         {
             nextBeatStartTime += beatTime;
+            StartCoroutine(BeatEffect(1));
         }
+
+        if(offBeatStartTime < Time.time - beatTime / 2)
+        {
+            offBeatStartTime += beatTime;
+            //StartCoroutine(BeatEffect(0.2f));
+        }
+
         timeBeforeNextBeat = nextBeatStartTime - Time.time;
+        currentBeatProgression = timeBeforeNextBeat / beatTime;
+    }
+
+    private IEnumerator BeatEffect(float amplitude)
+    {
+        mainCamera.orthographicSize = initialCameraSize + cameraBeatEffectAmplitude * amplitude;
+        while(mainCamera.orthographicSize > initialCameraSize + 0.01f)
+        {
+            mainCamera.orthographicSize -= cameraBeatEffectLerpSpeed * (mainCamera.orthographicSize - initialCameraSize) * Time.fixedDeltaTime * 50;
+            yield return new WaitForFixedUpdate();
+        }
+        mainCamera.orthographicSize = initialCameraSize;
+    }
+
+    public bool OnBeat()
+    {
+        bool onBeat = false;
+
+        /*if(timeBeforeNextBeat < timingThreshold / 2 || beatTime - timeBeforeNextBeat < timingThreshold / 2)
+        {
+            onBeat = true;
+        }*/
+
+        if(Time.time > offBeatStartTime - (timingThreshold / 2) + timingThresholdOffset && Time.time < offBeatStartTime + (timingThreshold / 2) + timingThresholdOffset)
+        {
+            return true;
+        }
+
+        return onBeat;
     }
 }
--- GPK Project/Assets/Scripts/Player/Blink.cs
+++ GPK Project/Assets/Scripts/Player/Blink.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 public class Blink : MonoBehaviour
 {
     #region Initialization
+    public float[] blinkRangeProgression;
+
+    public GameObject timingEffectPrefab;
+    public BeatManager beatManager;
+
+    private float currentRange;
+    private int currentTimedCombo;
     private Vector2 worldMousePos;
     private Hook selectedHook = null;
 
+    private LineRenderer lineCircle;
     #endregion
 
 
     void Start()
     {
-
+        currentTimedCombo = 0;
+        lineCircle = GetComponent<LineRenderer>();
+        currentRange = blinkRangeProgression[0];
     }
 
 
     void Update()
     {
         #region Hover mouse
+        DrawCircle(currentRange, transform.position);
+
         worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         Collider2D[] hookHover = Physics2D.OverlapPointAll(worldMousePos, LayerMask.GetMask("Hook"));
@@ -27,7 +39,7 @@ public class Blink : MonoBehaviour
         {
             float distanceToHook;
             distanceToHook = Vector2.Distance(hookHover[i].transform.position, worldMousePos);
-            if (distanceToHook < minDistanceToHook)
+            if (distanceToHook < minDistanceToHook && Vector2.Distance(transform.position, hookHover[i].transform.position) <= currentRange && transform.position != hookHover[i].transform.position)
             {
                 minDistanceToHook = distanceToHook;
                 if (selectedHook != null)
@@ -49,6 +61,32 @@ public class Blink : MonoBehaviour
         if (Input.GetButtonDown("Blink") && selectedHook != null)
         {
             transform.position = selectedHook.transform.position;
+            if(beatManager.OnBeat())
+            {
+                currentTimedCombo += 1;
+                Instantiate(timingEffectPrefab, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                currentTimedCombo = 0;
+            }
+            selectedHook.selected = false;
+            selectedHook = null;
         }
+
+
+        currentRange = blinkRangeProgression[currentTimedCombo < blinkRangeProgression.Length ? currentTimedCombo : blinkRangeProgression.Length - 1];
+    }
+
+    private void DrawCircle(float radius, Vector2 center)
+    {
+        Vector3[] circleLinePos = new Vector3[50];
+        for (int i = 0; i < circleLinePos.Length; i++)
+        {
+            circleLinePos[i] = new Vector2(Mathf.Cos(((2 * Mathf.PI) / 50) * i), Mathf.Sin(((2 * Mathf.PI) / 50) * i));
+            circleLinePos[i] *= radius;
+            circleLinePos[i] += (Vector3)center;
+        }
+        lineCircle.SetPositions(circleLinePos);
     }
 }
